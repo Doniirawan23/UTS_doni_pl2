bfe5b3d465c2d0d73e265626b029a4359e61a2ee
Doni Irawan <89141790+Doniirawan23@users.noreply.github.com>
2021-09-18 09:08:23 +0700

    Add files via upload

---
+++ UTS_PL2/uts_doni_pl2.sql
@@ -0,0 +1,225 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 18, 2021 at 04:07 AM
+-- Server version: 10.4.13-MariaDB
+-- PHP Version: 7.4.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `uts_doni_pl2`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_kelas`
+--
+
+CREATE TABLE `tbl_kelas` (
+  `id_kelas` int(11) NOT NULL,
+  `nama_kelas` varchar(10) NOT NULL,
+  `kopetensi_keahlian` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_kelas`
+--
+
+INSERT INTO `tbl_kelas` (`id_kelas`, `nama_kelas`, `kopetensi_keahlian`) VALUES
+(1, 'C', 'Sistem Informasi');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_pembayaran`
+--
+
+CREATE TABLE `tbl_pembayaran` (
+  `id_pembayaran` int(11) NOT NULL,
+  `id_petugas` int(11) NOT NULL,
+  `nisn` int(10) NOT NULL,
+  `tgl_bayar` int(2) NOT NULL,
+  `bulan_dibayar` varchar(10) NOT NULL,
+  `tahun_dibayar` int(4) NOT NULL,
+  `id_spp` int(11) NOT NULL,
+  `jumlah_bayar` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_pembayaran`
+--
+
+INSERT INTO `tbl_pembayaran` (`id_pembayaran`, `id_petugas`, `nisn`, `tgl_bayar`, `bulan_dibayar`, `tahun_dibayar`, `id_spp`, `jumlah_bayar`) VALUES
+(1, 1, 14197011, 10, '10', 2021, 1, 500000);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_petugas`
+--
+
+CREATE TABLE `tbl_petugas` (
+  `id_petugas` int(11) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `nama_petugas` varchar(20) NOT NULL,
+  `level` enum('Administrator','Petugas','') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_petugas`
+--
+
+INSERT INTO `tbl_petugas` (`id_petugas`, `username`, `password`, `nama_petugas`, `level`) VALUES
+(1, 'doni23', 'doni23', 'Doni_Irawan', 'Administrator');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_siswa`
+--
+
+CREATE TABLE `tbl_siswa` (
+  `nisn` int(10) NOT NULL,
+  `nis` int(10) NOT NULL,
+  `nama` varchar(20) NOT NULL,
+  `id_kelas` int(11) NOT NULL,
+  `alamat` text NOT NULL,
+  `no_telp` varchar(13) NOT NULL,
+  `id_spp` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_siswa`
+--
+
+INSERT INTO `tbl_siswa` (`nisn`, `nis`, `nama`, `id_kelas`, `alamat`, `no_telp`, `id_spp`) VALUES
+(14197011, 2323, 'Doni Irawan', 1, 'Jalan Lawang Gintung Skip', '083811442377', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_spp`
+--
+
+CREATE TABLE `tbl_spp` (
+  `id_spp` int(11) NOT NULL,
+  `tahun` int(4) NOT NULL,
+  `nominal` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_spp`
+--
+
+INSERT INTO `tbl_spp` (`id_spp`, `tahun`, `nominal`) VALUES
+(1, 2021, 500000);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_kelas`
+--
+ALTER TABLE `tbl_kelas`
+  ADD PRIMARY KEY (`id_kelas`);
+
+--
+-- Indexes for table `tbl_pembayaran`
+--
+ALTER TABLE `tbl_pembayaran`
+  ADD PRIMARY KEY (`id_pembayaran`),
+  ADD KEY `id_petugas` (`id_petugas`),
+  ADD KEY `nisn` (`nisn`),
+  ADD KEY `id_spp` (`id_spp`);
+
+--
+-- Indexes for table `tbl_petugas`
+--
+ALTER TABLE `tbl_petugas`
+  ADD PRIMARY KEY (`id_petugas`);
+
+--
+-- Indexes for table `tbl_siswa`
+--
+ALTER TABLE `tbl_siswa`
+  ADD PRIMARY KEY (`nisn`),
+  ADD UNIQUE KEY `id_spp` (`id_spp`),
+  ADD KEY `id_kelas` (`id_kelas`);
+
+--
+-- Indexes for table `tbl_spp`
+--
+ALTER TABLE `tbl_spp`
+  ADD PRIMARY KEY (`id_spp`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_kelas`
+--
+ALTER TABLE `tbl_kelas`
+  MODIFY `id_kelas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `tbl_pembayaran`
+--
+ALTER TABLE `tbl_pembayaran`
+  MODIFY `id_pembayaran` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `tbl_petugas`
+--
+ALTER TABLE `tbl_petugas`
+  MODIFY `id_petugas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `tbl_siswa`
+--
+ALTER TABLE `tbl_siswa`
+  MODIFY `nisn` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14197012;
+
+--
+-- AUTO_INCREMENT for table `tbl_spp`
+--
+ALTER TABLE `tbl_spp`
+  MODIFY `id_spp` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `tbl_pembayaran`
+--
+ALTER TABLE `tbl_pembayaran`
+  ADD CONSTRAINT `tbl_pembayaran_ibfk_1` FOREIGN KEY (`nisn`) REFERENCES `tbl_siswa` (`nisn`),
+  ADD CONSTRAINT `tbl_pembayaran_ibfk_2` FOREIGN KEY (`id_spp`) REFERENCES `tbl_siswa` (`id_spp`),
+  ADD CONSTRAINT `tbl_pembayaran_ibfk_3` FOREIGN KEY (`id_petugas`) REFERENCES `tbl_petugas` (`id_petugas`);
+
+--
+-- Constraints for table `tbl_siswa`
+--
+ALTER TABLE `tbl_siswa`
+  ADD CONSTRAINT `tbl_siswa_ibfk_1` FOREIGN KEY (`id_kelas`) REFERENCES `tbl_kelas` (`id_kelas`),
+  ADD CONSTRAINT `tbl_siswa_ibfk_2` FOREIGN KEY (`id_spp`) REFERENCES `tbl_spp` (`id_spp`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
